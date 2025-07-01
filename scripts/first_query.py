688cd31499b194361421bc648313be9854a0f3fe
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2025-07-01 13:44:18 +0200

    added 'if __name__ == "__main__":'

--- scripts/first_query.py
+++ scripts/first_query.py
@@ -7,16 +7,17 @@ from sqlalchemy.orm import Session
 # Query db using environment variable
 engine = create_engine(getenv("DATABASE_URL"))
 
-
 def run_firstquery() -> None:
 
     config=load_config()
-
     with Session(engine) as session:
         results = session.query(KBRGravimetry).limit(5).all()
-
         print('\t'.join(config['SATELLITE_FIELDS']))
         for row in results:
             for f in config['SATELLITE_FIELDS']:
                 print(getattr(row,f), end='\t', flush=True)
             print('\b', flush=True)
+
+
+if __name__ == "__main__":
+    run_firstquery()
