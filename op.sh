ad816a75c7aca30324d390f043956838b45dbbb1
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2025-09-19 16:54:14 +0200

    added env operation

--- op.sh
+++ op.sh
@@ -40,6 +40,12 @@ do
     pipx install poetry
     poetry -V
   ;;
+  env) #operation: show the values of the variables in the .env file
+    for i in $(cat $DIR/.env | sed 's:#.*::g' | sed '/^$/d' | awk -F= '{print $1}')
+    do
+      eval "echo ${$i}"
+    done
+  ;;
   down) #operation: turn off the db
     podman-compose down
   ;;
