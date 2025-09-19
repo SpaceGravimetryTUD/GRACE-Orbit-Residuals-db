46bae4c37d23bd00261c69dd741228bf4c88ad53
Joao De Teixeira da Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2025-09-19 16:53:24 +0200

    minor improvement

--- clean-pyenv-from-path.sh
+++ clean-pyenv-from-path.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -ue
 
+STRING=.pyenv
+
 #check if this script was sourced (https://stackoverflow.com/a/28776166)
-if (return 0 2>/dev/null) 
+if (return 0 2>/dev/null)
 then
-  echo "=== Before:"
-  echo $PATH | awk -F: '{for(i=1; i<=NF; i++) {printf("%s\n",$i)}}'
+  if grep -q $STRING  <(echo $PATH)
+  then
+    echo "=== Before:"
+    echo $PATH | awk -F: '{for(i=1; i<=NF; i++) {printf("%s\n",$i)}}'
 
-  export PATH=$(echo $PATH | awk -F: '{for(i=1; i<=NF; i++) {printf("%s\n",$i)}}' | grep -v .pyenv | tr '\n' ':' | sed 's/:$/\n/')
+    export PATH=$(
+      echo $PATH \
+      | awk -F: '{for(i=1; i<=NF; i++) {printf("%s\n",$i)}}' \
+      | grep -v $STRING \
+      | tr '\n' ':' | sed 's/:$/\n/' \
+    )
 
-  echo "=== After:"
-  echo $PATH | awk -F: '{for(i=1; i<=NF; i++) {printf("%s\n",$i)}}'
+    echo "=== After:"
+    echo $PATH | awk -F: '{for(i=1; i<=NF; i++) {printf("%s\n",$i)}}'
+  else
+    echo "NOTICE: PATH already clean of entries containing '$STRING'"
+  fi
 else
   echo "ERROR: this script needs to be sourced"
   exit 3
